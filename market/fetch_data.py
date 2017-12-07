2cabe056534206195293b41f171b28fce8b57354
renxiliang <renxiliang@bilibili.com>
2017-12-07 17:04:33 +0800

    update

--- market/fetch_data.py
+++ market/fetch_data.py
@@ -1,49 +1,63 @@
 # -*- coding: utf-8 -*-
 # -*- author: nik -*-
 import matplotlib.pyplot as plt
-import pandas as pd
 import numpy as np
 import tushare as ts
 
-import sklearn
-#获取今天分笔数据
-today_ticks = ts.get_today_ticks('601313')
-print(today_ticks.info())
-print(today_ticks)
+class FetchData:
 
-#获取实时分笔数据
-realtime = ts.get_realtime_quotes('601313')
-print(realtime.info())
-print(realtime)
+    def __init__(self, code):
+        self.code = code
 
-#获得实时行情数据
-#all = ts.get_today_all()
-#print(all.head())
+    def desc_data(self, data_frame):
+        print("\n---------------desc data start ------------------------\n")
+        print(data_frame.info())
+        print("\n-----------------------------------------------\n")
+        print(data_frame.head())
+        print("\n-----------------------------------------------\n")
+        print(data_frame.tail())
 
-#获得历史数据 江南嘉捷
-data = ts.get_hist_data('601313')
-#data = data.sort_values(by='date',ascending=True)
-print(data.info())
-print(data.head())
-print(data.head(-5))
-data.to_csv("hist_data.csv")
-exit(0)
-# print(data.shape)
+        print("\n----------------desc data end-------------------------\n")
+    '''
+    #获取今天分笔数据
+    '''
+    def get_today_ticks(self):
+        #获取今天分笔数据
+        today_ticks = ts.get_today_ticks(self.code)
+        self.desc_data(today_ticks)
+        today_ticks.to_csv("today_data.csv")
+    '''
+    #获取实时分笔数据
+    '''
+    def get_realtime_quotes(self):
+        #获取实时分笔数据
+        realtime = ts.get_realtime_quotes(self.code)
+        self.desc_data(realtime)
+        realtime.to_csv("realtime_data.csv")
+    '''
+    #获得所有发行股票实时行情数据
+    '''
+    def get_today_all(self):
+        #获得所有发行股票实时行情数据
+        all = ts.get_today_all()
+        self.desc_data(all)
+        all.to_csv("today_all.csv")
+    '''
+    获得历史交易数据
+    '''
+    def get_hist_data(self):
+        #获得历史数据
+        data = ts.get_hist_data(self.code)
+        #data = data.sort_values(by='date',ascending=True)
+        self.desc_data(data)
+        data.to_csv("hist_data.csv")
 
-ax1 = plt.subplot(211)
-t1 = np.arange(630)
 
-#reverse the data
-plt.plot(t1, data.open[::-1], label="open")
-plt.plot(t1, data.high[::-1], label="max")
-plt.plot(t1, data.close[::-1], label="close")
-plt.plot(t1, data.low[::-1], label="min")
-#plt.bar(t1,data.volume,label="volume")
+if __name__ == "__main__":
+    fd = FetchData("601313")
+    fd.get_hist_data()
+    fd.get_today_all()
+    fd.get_realtime_quotes()
+    fd.get_today_ticks()
 
-#plt.xlim(1,10)
-leg = plt.legend(loc='upper left', ncol=4, mode="expand", shadow=True, fancybox=True)
-leg.get_frame().set_alpha(0.5)
-ax2 = plt.subplot(212)
-plt.bar(t1,data.volume[::-1],label="volume")
 
-plt.show()
--- market/market.py
+++ market/market.py
@@ -3,18 +3,11 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
-import tushare as ts
 
-import sklearn
+
 
 #获得历史数据 江南嘉捷
 data = pd.read_csv("hist_data.csv")
-
-#data = data.sort_values(by='date',ascending=True)
-print(data.info())
-print(data.head())
-print(data.head(-5))
-# print(data.shape)
 
 ax1 = plt.subplot(311)
 t1 = np.arange(630)
@@ -24,7 +17,7 @@ plt.plot(t1, data.open[::-1], label="open")
 plt.plot(t1, data.high[::-1], label="max")
 plt.plot(t1, data.close[::-1], label="close")
 plt.plot(t1, data.low[::-1], label="min")
-#plt.bar(t1,data.volume,label="volume")
+
 
 #plt.xlim(1,10)
 leg = plt.legend(loc='upper left', ncol=4, mode="expand", shadow=True, fancybox=True)
@@ -33,8 +26,49 @@ ax2 = plt.subplot(312)
 plt.bar(t1,data.volume[::-1],label="volume")
 
 ax3 = plt.subplot(313)
-plt.scatter(data.open[::-1],data.close[::-1])
-plt.show()
+plt.scatter(data.volume[::-1],data.close[::-1])
+#plt.show()
 
+##############
+#数据准备
+y = data.close.values.reshape(-1,1)
+#drop column close
+X = data.drop('close',axis=1)
+X = data.drop('date',axis=1)
+print(y.shape)
+print(X.shape)
 
+from sklearn.model_selection import train_test_split
+# 依然如故，我们对数据进行分割
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state=33)
 
+from sklearn.preprocessing import StandardScaler
+
+# 正规化的目的在于避免原始特征值差异过大，导致训练得到的参数权重不一
+scalerX = StandardScaler().fit(X_train)
+X_train = scalerX.transform(X_train)
+X_test = scalerX.transform(X_test)
+
+scalery = StandardScaler().fit(y_train)
+y_train = scalery.transform(y_train)
+y_test = scalery.transform(y_test)
+
+# 先把评价模块写好，依然是默认5折交叉验证，只是这里的评价指标不再是精度，而是另一个函数R2，大体上，这个得分多少代表有多大百分比的回归结果可以被训练器覆盖和解释
+from sklearn.model_selection import KFold,cross_val_score
+
+def train_and_evaluate(clf, X_train, y_train):
+    cv = KFold( n_splits = 5, shuffle=True, random_state=33)
+    scores = cross_val_score(clf, X_train, y_train, cv=cv)
+    print ('Average coefficient of determination using 5-fold cross validation:', np.mean(scores))
+
+# 线性模型尝试， SGD_Regressor
+from sklearn import linear_model
+# 这里有一个正则化的选项penalty，目前14维特征也许不会有太大影响
+clf_sgd = linear_model.SGDRegressor(loss='squared_loss', penalty=None, random_state=42)
+train_and_evaluate(clf_sgd, X_train, y_train)
+
+# 再看看SVM的regressor怎么样（都是默认参数）,
+from sklearn.svm import SVR
+# 使用线性核没有啥子提升，但是因为特征少，所以可以考虑升高维度
+clf_svr = SVR(kernel='linear')
+train_and_evaluate(clf_svr, X_train, y_train)
