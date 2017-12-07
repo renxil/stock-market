c54534f3839698f47bde27902f31ccff38185733
renxiliang <renxiliang@bilibili.com>
2017-12-07 17:06:55 +0800

    update

--- market/market.py
+++ market/market.py
@@ -67,8 +67,26 @@ from sklearn import linear_model
 clf_sgd = linear_model.SGDRegressor(loss='squared_loss', penalty=None, random_state=42)
 train_and_evaluate(clf_sgd, X_train, y_train)
 
+# 再换一个SGD_Regressor的penalty参数为l2,结果貌似影响不大，因为特征太少，正则化意义不大
+clf_sgd_l2 = linear_model.SGDRegressor(loss='squared_loss', penalty='l2', random_state=42)
+train_and_evaluate(clf_sgd_l2, X_train, y_train)
 # 再看看SVM的regressor怎么样（都是默认参数）,
 from sklearn.svm import SVR
 # 使用线性核没有啥子提升，但是因为特征少，所以可以考虑升高维度
 clf_svr = SVR(kernel='linear')
 train_and_evaluate(clf_svr, X_train, y_train)
+
+clf_svr_poly = SVR(kernel='poly')
+# 升高维度，效果明显，但是此招慎用@@，特征高的话, CPU还是受不了，内存倒是小事。其实到了现在，连我们自己都没办法直接解释这些特征的具体含义了。
+train_and_evaluate(clf_svr_poly, X_train, y_train)
+
+clf_svr_rbf = SVR(kernel='rbf')
+# RBF (径向基核更是牛逼！)
+train_and_evaluate(clf_svr_rbf, X_train, y_train)
+# 再来个更猛的! 极限回归森林，放大招了！！！
+from sklearn import ensemble
+clf_et = ensemble.ExtraTreesRegressor()
+train_and_evaluate(clf_et, X_train, y_train)
+# 最后看看在测试集上的表现
+clf_et.fit(X_train, y_train)
+clf_et.score(X_test, y_test)
